4827d872fdae71a5c558d5254a0b188f7b7fa1ac
taesikyoo <yrns0907@gmail.com>
2020-12-22 17:07:10 +0900

    initial commit

---
+++ src/codingtest/q0304.java
@@ -0,0 +1,51 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class q0304 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int answer = 0;
+
+        while (n != 1) {
+            if (n % k == 0) {
+                n /= k;
+                answer++;
+                continue;
+            }
+            n -= 1;
+            answer ++;
+        }
+        System.out.println(answer);
+    }
+
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+
+        // N, K를 공백을 기준으로 구분하여 입력 받기
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int result = 0;
+
+        while (true) {
+            // N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+            int target = (n / k) * k;
+            result += (n - target);
+            n = target;
+            // N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+            if (n < k) break;
+            // K로 나누기
+            result += 1;
+            n /= k;
+        }
+
+        // 마지막으로 남은 수에 대하여 1씩 빼기
+        result += (n - 1);
+        System.out.println(result);
+    }
+}
---
+++ src/codingtest/q0402.java
@@ -0,0 +1,27 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class q0402 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= 59; j++) {
+                for (int k = 0; k <= 59; k++) {
+                    String hour = String.valueOf(i);
+                    String min = String.valueOf(j);
+                    String sec = String.valueOf(k);
+                    if(hour.contains("3") || min.contains("3") || sec.contains("3")) count++;
+                    // String time = hour + min + sec; time.contains("3)
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
