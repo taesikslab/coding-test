43a3b08f8aa9b294310b7b11d67f81a329093ee0
taesikyoo <yrns0907@gmail.com>
2020-12-31 14:25:13 +0900

    8장 다이나믹 프로그래밍

---
+++ src/codingtest/q08/q0806.java
@@ -0,0 +1,26 @@
+package codingtest.q08;
+
+import java.util.Scanner;
+
+public class q0806 {
+
+    public static int[] d = new int[100];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] storage = new int[n];
+        for (int i = 0; i < n; i++) {
+            storage[i] = sc.nextInt();
+        }
+
+        d[0] = storage[0];
+        d[1] = Math.max(d[1], storage[1]);
+        for (int i = 2; i < n; i++) {
+            d[i] = Math.max(d[i - 1], (d[i - 2] + storage[i]));
+        }
+
+        System.out.println(d[n - 1]);
+    }
+}
