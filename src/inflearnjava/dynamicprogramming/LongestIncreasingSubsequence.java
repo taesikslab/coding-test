55dada97c64f5977edbaa21aebc903f1b33aa20b
taesikyoo <yrns0907@gmail.com>
2021-02-08 10:28:17 +0900

    [LeetCode] Dynamic Programming

---
+++ src/inflearnjava/dynamicprogramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,32 @@
+package inflearnjava.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 2, 5, 2, 6, 10, 4, 12};
+
+        System.out.println(solve(nums));
+    }
+
+    public static int solve(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int result = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+}
