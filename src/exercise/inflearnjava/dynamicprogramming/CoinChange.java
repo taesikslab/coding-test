85bfd61b7ab22bee3092b5f926177280c58612f4
taesikyoo <yrns0907@gmail.com>
2021-02-23 14:46:42 +0900

    [LeetCode] Dynamic Programming 연습

---
+++ src/exercise/inflearnjava/dynamicprogramming/CoinChange.java
@@ -0,0 +1,29 @@
+package exercise.inflearnjava.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println(coinChange(coins, amount));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+}
