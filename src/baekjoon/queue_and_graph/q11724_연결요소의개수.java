b10da8fd5c86e4fa4f7b8d0a449670f7cd6ec7d1
taesikyoo <yrns0907@gmail.com>
2021-03-22 23:43:38 +0900

    [Baekjoon] 큐와 그래프

---
+++ src/baekjoon/queue_and_graph/q11724_연결요소의개수.java
@@ -0,0 +1,46 @@
+package baekjoon.queue_and_graph;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class q11724_연결요소의개수 {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            a[u].add(v);
+            a[v].add(u);
+        }
+        boolean[] check = new boolean[n + 1];
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (check[i] == false) {
+                dfs(a, check, i);
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+
+
+    }
+
+    public static void dfs(ArrayList<Integer>[] a, boolean[] c, int x) {
+        if (c[x]) {
+            return;
+        }
+        c[x] = true;
+        for (int y : a[x]) {
+            if (c[y] == false) {
+                dfs(a, c, y);
+            }
+        }
+    }
+}
